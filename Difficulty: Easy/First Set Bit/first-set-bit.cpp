9ab918b2fc1715ce485f46b48e848ccbed885ad8
Koushik Pathuri <119720431+pathurikoushik@users.noreply.github.com>
2025-03-07 11:50:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Set Bit/first-set-bit.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+class Solution {
+  public:
+    // Function to find position of first set bit in the given number.
+    int getFirstSetBit(int n) {
+        // Your code here
+        if (n == 0) return 0; 
+        
+        return log2(n & -n) + 1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver code
+int main() {
+    int t;
+    cin >> t; // testcases
+    while (t--) {
+        int n;
+        cin >> n; // input n
+        Solution ob;
+        printf("%u\n", ob.getFirstSetBit(n)); // function to get answer
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
